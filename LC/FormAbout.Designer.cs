cd6eac8590092cefd1ce623b96ed76450f82ec94
mutex81 <mutex81@yandex.ru>
2018-02-13 09:59:33 +0300

    Провожу изменения с другого рабочего места.

--- LC/FormAbout.Designer.cs
+++ LC/FormAbout.Designer.cs
@@ -100,7 +100,7 @@
             this.buttonOk.Name = "buttonOk";
             this.buttonOk.Size = new System.Drawing.Size(137, 34);
             this.buttonOk.TabIndex = 5;
-            this.buttonOk.Text = "Мне все ясно";
+            this.buttonOk.Text = "Ясно";
             this.buttonOk.UseVisualStyleBackColor = true;
             this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
             // 
